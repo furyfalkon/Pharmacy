9504c59c1cf7ed73eb45db6242e070d8b8fd3cea
F.W <fjowo@posteo.de>
2026-01-12 12:21:26 +0100

    Lagersystem erweitert

--- src/main/java/gamelogic/Lager.java
+++ src/main/java/gamelogic/Lager.java
@@ -30,8 +30,65 @@ public class Lager {
 
         return items[pos];
     }
+    /**
+     * @param ziehlLager lager in welches die Items verschoben werden sollen
+     * @param  sourceLager lager aus dem die Items entnommen werden
+     * @param pos Position des zu verschiebenden Items im Source lager
+    Items werden aus einem Source Lager in den ersten Slot eines Zieh-lagers verschoben, in welchem dieses bereits vorhanden ist,
+            falls das item noch nicht vorhanden ist, wird der erste freie Slot verwendet.*/
+    public void transferItem(Lager ziehlLager,Lager sourceLager,int pos) {
 
-    public void addItem(Lager lager) {
-        lager.getItems();
+        for (int i = 0; i < ziehlLager.items.length; i++) {
+            if (ziehlLager.items[i] == sourceLager.items[pos]) {
+                ziehlLager.Amount[i] = ziehlLager.Amount[i]+sourceLager.Amount[pos];
+                emtyPos(sourceLager,pos);
+                return ;
+            }
+        }
+        for (int i = 0; i < ziehlLager.items.length; i++) {
+            if (ziehlLager.items[i] == null) {
+                ziehlLager.items[i]=sourceLager.items[pos];
+                ziehlLager.Amount[i] =sourceLager.Amount[pos];
+                emtyPos(sourceLager,pos);
+                return ;
+            }
+        }
     }
+    /**
+     * @param ziehlLager lager in welches die Items verschoben werden sollen
+     * @param  sourceLager lager aus dem die Items entnommen werden
+     * @param sourcePos Position des zu verschiebenden Items im Source lager
+     * @param ziehlPos Position in die Verschoben wird
+    Ein Item wird aus einem Source Lager in den ausgewählten Slot im Ziel-Lager verschoben.*/
+    public void addOneItem(Lager ziehlLager,Lager sourceLager,int sourcePos,int ziehlPos) {
+        if(ziehlLager.items[ziehlPos]==sourceLager.items[sourcePos]){
+            if (sourceLager.Amount[sourcePos]>0) {
+                ziehlLager.Amount[ziehlPos]++;
+                sourceLager.Amount[ziehlPos]--;
+            }
+            if (sourceLager.Amount[sourcePos]==0){
+            emtyPos(sourceLager,sourcePos);}
+        }
+        if(ziehlLager.items[ziehlPos]==null){
+            if (sourceLager.Amount[sourcePos]>0) {
+                ziehlLager.items[ziehlPos]=sourceLager.items[sourcePos];
+                ziehlLager.Amount[ziehlPos]++;
+                sourceLager.Amount[ziehlPos]--;
+            }
+            if (sourceLager.Amount[sourcePos]==0){
+                emtyPos(sourceLager,sourcePos);}
+        }
+
+
+    }
+
+    private void emtyPos(Lager lager,int pos){
+        lager.Amount[pos]=0;
+        lager.items[pos]=null;
+    }
+
 }
+
+
+
+
